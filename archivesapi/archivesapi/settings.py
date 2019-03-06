a6e4fe6bda8ce4a37c37c4a648f8514ec7919a9f
Bryan Mutai <mutaiwork@gmail.com>
2019-03-06 15:06:55 +0300

    add cors stuff for the API

--- archivesapi/archivesapi/settings.py
+++ archivesapi/archivesapi/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_s3_storage',
     'archives'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +156,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
